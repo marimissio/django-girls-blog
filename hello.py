1e5e28c73492410ed236538ff04289a4d2de16ee
marimissio <mari.missio@gmail.com>
2018-11-03 17:39:35 -0300

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+#greetings
+def hi(nome):
+    if nome == "Mari":
+        print('Hello ' + nome + '!')
+        print('How are you?')
+    elif nome == "Mare":
+        print("Hello!")
+        print("But your name is spelled wrong")
+    else:
+        print("Hello Stranger")
+
+hi("Mari")
+
